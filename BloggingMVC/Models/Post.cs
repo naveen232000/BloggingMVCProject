e68a1cb07759ada1da800facaddd01a5e16d18fb
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-02 15:26:27 +0530

    Add project files.

---
+++ BloggingMVC/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggingMVC.Models
+{
+    [Table("Post")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Auther {  get; set; }
+
+
+    }
+}
